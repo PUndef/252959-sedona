60057c1fcd2ccc1b51f0d03d8eac94e99c3cd3d8
pzyryanov1995 <pzyryanov1995@gmail.com>
2017-02-20 19:11:52 +0700

    Исправил работу скрипта

--- js/script.js
+++ js/script.js
@@ -1,18 +1,12 @@
-var button = document.querySelector(".search-hotel-description-top");
-var popup = document.querySelector(".modal-content");
+var btn = document.querySelector(".search_btn");
+var popup = document.querySelector(".blank-search-hotel");
 var form = popup.querySelector("form");
 var dateArr = popup.querySelector(".date-arrival");
 var dateDep = popup.querySelector(".date-departure");
-var map = document.querySelector(".map-window")
-var adult = localStorage.getItem(".count-adult");
-var child = localStorage.getItem(".count-child");
 
-button.addEventListener("click", function (event) {
+btn.addEventListener("click", function (event) {
     event.preventDefault();
-    popup.classList.remove("modal-error")
-    popup.classList.toggle("modal-content-show");
-    map.classList.toggle("map-window-hide")
-    dateArr.focus();
+    popup.classList.toggle("open");
 });
 
 form.addEventListener("submit", function (event) {
