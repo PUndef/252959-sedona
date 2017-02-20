12f457af6b1440835c2d6476ec3d9fd96a037839
pzyryanov1995 <pzyryanov1995@gmail.com>
2017-02-20 14:32:18 +0700

    Добавил анимацию и карту

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 var button = document.querySelector(".search-hotel-description-top");
 var popup = document.querySelector(".modal-content");
-var date = popup.querySelector(".date-arrival")
+var form = popup.querySelector("form");
+var dateArr = popup.querySelector(".date-arrival");
+var dateDep = popup.querySelector(".date-departure");
+var map = document.querySelector(".map-window")
+var adult = localStorage.getItem(".count-adult");
+var child = localStorage.getItem(".count-child");
 
-button.addEventListener("click", function(event)   {
+button.addEventListener("click", function (event) {
     event.preventDefault();
+    popup.classList.remove("modal-error")
     popup.classList.toggle("modal-content-show");
-    date.focus()
+    map.classList.toggle("map-window-hide")
+    dateArr.focus();
+});
+
+form.addEventListener("submit", function (event) {
+     if (!dateArr.value || !dateDep.value)    {
+       event.preventDefault();
+       popup.classList.toggle("modal-error");
+     }
 });
